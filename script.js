1d85031afece96bdf4df7bcf71f069f3002a4b2b
vikash8769 <ucet.itvikashranjan2028@gmail.com>
2025-07-19 08:21:09 +0530

    second commit

---
+++ script.js
@@ -0,0 +1,48 @@
+let timerDisplay = document.querySelector('.timerDisplay');
+let stopBtn = document.getElementById('StopBtn');
+let resetBtn = document.getElementById('resetBtn');
+let startBtn = document.getElementById('startBtn');
+
+let msec = 0;
+let secs = 0;
+let mins = 0;
+
+let timerId = null;
+
+startBtn.addEventListener('click', function(){
+    if(timerId !== null){
+        clearInterval(timerId);
+    }
+    timerId = setInterval(startTimer, 10);
+});
+
+stopBtn.addEventListener('click', function(){
+    clearInterval(timerId);
+});
+
+resetBtn.addEventListener('click', function(){
+    clearInterval(timerId);
+    timerDisplay.innerHTML = '00 : 00 : 00';
+    msec = secs = mins = 0;
+});
+
+function startTimer(){
+    msec++;
+    if(msec == 100){
+        msec = 0;
+        secs++;
+        if(secs == 60){
+            secs = 0;
+mins++;
+        }
+    }
+
+    let msecString = msec < 10 ? '0${msec}' : msec;
+    let seccString = secs < 10 ? '0${msec}' : secs;
+    let minsString = mins < 10 ? '0${msec}' : mins;
+
+
+    timerDisplay.innerHTML = '${minsString} :  ${secsString} : ${msecString}';
+
+    }
+
